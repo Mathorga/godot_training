e321ba47aa1942957e069de85ed5ec824f64b165
Luka Micheletti <michelettiluka@gmail.com>
2026-01-14 01:18:15 +0100

    Added fall off to foxy

--- fox_antics/scenes/player/player.gd
+++ fox_antics/scenes/player/player.gd
@@ -5,8 +5,12 @@ class_name Player
 const GRAVITY: Vector2 = Vector2(0.0, 981.0)
 const JUMP_SPEED: float = 350.0
 const RUN_SPEED: float = 100.0
+const MAX_FALL_SPEED: float = 500.0
+
+@export var fall_off_y: float = 1000.0 
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var debug_label: Label = $DebugLabel
 
 func _physics_process(delta: float) -> void:
 	# Apply gravity.
@@ -23,11 +27,29 @@ func _physics_process(delta: float) -> void:
 
 	flip_sprite()
 
+	velocity.y = clampf(velocity.y, -JUMP_SPEED, MAX_FALL_SPEED)
+
 	# Only apply movement after velocity has been updated.
 	move_and_slide()
+
+	update_debug_label()
+
+	check_fall_off()
 
 func flip_sprite() -> void:
 	if is_zero_approx(velocity.x):
 		return
 
 	sprite_2d.flip_h = velocity.x < 0
+
+func update_debug_label() -> void:
+	var debug_string: String = ""
+	debug_string += "floor: %s \n" % is_on_floor()
+	debug_string += "v: %.1f | %.1f\n" % [velocity.x, velocity.y]
+	debug_string += "p: %.1f | %.1f" % [global_position.x, global_position.y]
+
+	debug_label.text = debug_string
+
+func check_fall_off() -> void:
+	if global_position.y > fall_off_y:
+		queue_free()
