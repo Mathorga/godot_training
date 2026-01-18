b44fe45a7ffb5b62658c96aa04f913020c0e9d24
Luka <michelettiluka@gmail.com>
2026-01-18 01:13:40 +0100

    Added frog to foxy

---
+++ fox_antics/scenes/enemies/frog_enemy/frog_enemy.gd
@@ -0,0 +1,59 @@
+extends EnemyBase
+
+const JUMP_VELOCITY: Vector2 = Vector2(100.0, -300.0)
+
+@onready var jump_timer: Timer = $JumpTimer
+
+var seen_player: bool = false
+var can_jump: bool = false
+
+func _ready() -> void:
+	super._ready()
+
+	jump_timer.timeout.connect(on_jump_timer_timeout)
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+
+	velocity += gravity * delta
+
+	try_jump()
+
+	move_and_slide()
+
+	if is_on_floor():
+		velocity.x = 0.0
+		animated_sprite_2d.play("idle")
+
+	face_player()
+
+func face_player() -> void:
+	animated_sprite_2d.flip_h = player_ref.global_position.x > global_position.x
+
+func try_jump() -> void:
+	if not seen_player:
+		return
+
+	if not is_on_floor():
+		return
+
+	if not can_jump:
+		return
+
+	velocity = Vector2(JUMP_VELOCITY.x if animated_sprite_2d.flip_h else -JUMP_VELOCITY.x, JUMP_VELOCITY.y)
+	can_jump = false
+	start_timer() 
+	animated_sprite_2d.play("jump")
+
+func start_timer() -> void:
+	jump_timer.start(randi_range(2, 4))
+
+func on_jump_timer_timeout() -> void:
+	can_jump = true
+
+func on_screen_entered() -> void:
+	if seen_player:
+		return
+
+	seen_player = true
+	start_timer()
